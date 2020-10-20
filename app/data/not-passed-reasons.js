64c152a293c572590dfb3a5ac9e6850e7460f6ce
Jesse Yuen <hello@jesseyuen.com>
2020-10-20 16:28:07 +0100

    new stuff

---
+++ app/data/not-passed-reasons.js
@@ -0,0 +1,11 @@
+module.exports = [
+    'Yet to complete the course',
+    'Left course before the end',
+    'Failed the course',
+    'Withheld: skills test not met',
+    'Withheld: standards not met',
+    'Withheld: standards & skills test not met',
+    'Skills test not taken (standards met)',
+    'Skills tests not taken (standards not met)',
+    'Course complete - standards and/or skills test not met'
+]
