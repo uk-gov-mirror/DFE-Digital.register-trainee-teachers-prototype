69eedb01d4a195f39fb8953de697428f56c4aa5b
Ed Horsford <mail@edwardhorsford.com>
2020-11-17 11:51:23 +0000

    Initial support for provider courses

--- app/data/training-route-data.js
+++ app/data/training-route-data.js
@@ -1,6 +1,6 @@
 let mainTrainingRoutes = {
-  "Assessment Only": {
-    name: "Assessment Only",
+  "Assessment only": {
+    name: "Assessment only",
     defaultEnabled: true,
     sections: [
       'programmeDetails',
