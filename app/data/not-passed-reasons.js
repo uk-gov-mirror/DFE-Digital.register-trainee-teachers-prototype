2320372809efb495b8296c5134d60c897b9949e1
Jesse Yuen <hello@jesseyuen.com>
2020-11-23 13:55:48 +0000

    mucking about with the reasons

--- app/data/not-passed-reasons.js
+++ app/data/not-passed-reasons.js
@@ -1,11 +1,15 @@
 module.exports = [
-    'Course complete - standards not met',
-    'Failed the course',
+    // 'Course complete - standards not met',
+    'Did not do the assessment',
+    'Did not meet the academic requirements',
+    //  (Did not pass academic exams)',
+    'Did not pass the assessment'
+    // 'Failed the course',
     // 'Left course before the end',
     // 'Skills test not taken (standards met)',
     // 'Skills tests not taken (standards not met)',
     // 'Withheld: skills test not met',
     // 'Withheld: standards & skills test not met',
-    'Withheld - standards not met',
-    'Yet to complete the course'
+    // 'Withheld - standards not met',
+    // 'Yet to complete the course'
 ]
