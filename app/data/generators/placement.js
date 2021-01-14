2ddfabf65d13940f1ab56b04dbfafe0383f0ed83
Jesse Yuen <hello@jesseyuen.com>
2021-01-14 16:28:48 +0000

    Adding support for placement start month

--- app/data/generators/placement.js
+++ app/data/generators/placement.js
@@ -5,19 +5,21 @@ module.exports = () => {
 
   const item = () => {
     const location = faker.helpers.randomize(placementSchools)
+    const startMonth = faker.helpers.randomize(['1','2','3','4','5','6','7','8','9','10','11','12'])
     const duration = faker.helpers.randomize(['1','2','4','8','10','12'])
 
     return {
       location,
+      startMonth,
       duration,
       id: faker.random.uuid()
     }
   }
 
   const count = weighted.select({
-    0: 0.5,
-    2: 0.4,
-    4: 0.1
+    0: 0.3,
+    1: 0.5,
+    2: 0.2
   })
   const items = []
   for (var i = 0; i < count; i++) {
