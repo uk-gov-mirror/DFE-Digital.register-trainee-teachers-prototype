7cf483bad0073f5447b7fb25cca0da8ac1601c7c
Jesse Yuen <hello@jesseyuen.com>
2021-03-23 10:26:43 +0000

    WIP to add start year for placements

--- app/data/generators/placement.js
+++ app/data/generators/placement.js
@@ -7,11 +7,13 @@ module.exports = (params) => {
   const item = () => {
     const location = faker.helpers.randomize(placementSchools)
     const startMonth = faker.helpers.randomize(['1','2','3','4','5','6','7','8','9','10','11','12'])
+    const startYear = faker.helpers.randomize(['2019','2020','2021'])
     const duration = faker.helpers.randomize(['1','2','4','8','10','12'])
 
     return {
       location,
       startMonth,
+      startYear,
       duration,
       id: faker.random.uuid()
     }
